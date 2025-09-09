debaded8789d868c8e86298cb66818ff927fcfe6
SauravRai18 <108250158+SauravRai18@users.noreply.github.com>
2025-09-09 14:42:49 +0530

    Add files via upload

---
+++ assign2a.py
@@ -0,0 +1,18 @@
+'''
+num=int(input("Enter a number:"))
+
+if num ==2 or num % == 0:
+print("even")
+
+else:
+    print("odd")
+    
+'''
+
+
+num = int(input("Enter a number:"))
+
+if num == 2 or num % 2 == 0:
+    print("even")
+else:
+    print("odd")
